d3d2b6a9d2c68ac9a3f684cb82b169a7f29c787f
Mateusz.Galasinski <mateusz.galasinski@protonmail.com>
2019-01-11 23:05:55 +0100

    added TryVote event

--- Contracts/contracts/VoteManager.sol
+++ Contracts/contracts/VoteManager.sol
@@ -19,6 +19,12 @@ contract VoteManager {
         uint indexed candidate
     );
 
+    event VoteTry (
+        uint indexed ballot,
+        uint indexed candidate,
+        bool indexed isAllowed
+    );
+
     struct Voter {
         bool isAllowed;
         bool hasVoted;
@@ -77,6 +83,7 @@ contract VoteManager {
     }
 
     function vote(uint ballotId, uint voterId, uint candidateId) public {
+        emit VoteTry(ballotId, candidateId, ballots[ballotId].voters[voterId].isAllowed);
         require(ballots[ballotId].voters[voterId].isAllowed, "There is no such voter.");
         require(!ballots[ballotId].voters[voterId].hasVoted, "This voter has already voted.");
 
